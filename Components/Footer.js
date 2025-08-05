a046daae452169fdf8a7437c5430c44098f0e0b9
fxjakaria04 <freelancerx2023@gmail.com>
2025-08-05 15:41:06 +0600

    footer component added

---
+++ Components/Footer.js
@@ -0,0 +1,13 @@
+import { MenubarItems } from '@/lib/menubarItems'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='w-full py-10'>
+      <div className="max-w-7xl mx-auto py-1 flex items-center justify-between">
+        <p>hello world</p>
+      </div>
+    </div>
+  )
+}
