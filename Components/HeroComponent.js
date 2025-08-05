17c73063f274045e1dac73241a6f6e6653603e26
fxjakaria04 <freelancerx2023@gmail.com>
2025-08-05 17:05:58 +0600

    working

--- Components/HeroComponent.js
+++ Components/HeroComponent.js
@@ -1,9 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function HeroComponent() {
   return (
-    <div className='h-[80vh] backgroundImage py-10'>
-      
+    <div className='HeroGraphic mt-12'>
+      <div className='h-[80vh] max-w-[100rem] mx-auto p-10 border border-gray-700 rounded-2xl grid items-center grid-cols-1 md:grid-cols-2'>
+        <div>
+            <h1 className='text-4xl'>Welcome To SudoMotion</h1>
+        </div>
+        <div className='flex items-center justify-center'>
+            <Image src="/Image/sudoMotionShort.png" alt="SudoMotion" className='w-80' height={10000} width={10000}/>
+        </div>
+      </div>
     </div>
   )
 }
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,13 +1,15 @@
 import { MenubarItems } from '@/lib/menubarItems'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
     <nav className="w-full text-black fixed top-1">
-        <div className="max-w-6xl bg-[#93ff892d] backBlur rounded-lg shadow-md border-foreground px-5 py-2 mx-auto flex items-center justify-between">
+        {/* <Image src="/public/Image/SudoMotion.png" alt='SudoMotion' height={1000} width={1000}/> */}
+        <div className="max-w-6xl bg-[#f1f1f113] backBlur rounded-lg shadow-md border-foreground px-5 py-2 mx-auto flex items-center justify-between">
             {MenubarItems?.map((item)=>(
-            <Link href={'/'} key={item.id} className="text-black text-sm font-semibold">{item.name}</Link>
+            <Link href={'/'} key={item.id} className="text-sm font-semibold text-white">{item.name}</Link>
             ))}
 
         </div>
