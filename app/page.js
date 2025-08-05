ffbd6246c79cd5931f898ca571d42c91b58a4197
jakaria Talukdar <freelancerx2023@gmail.com>
2025-08-05 19:19:45 +0600

    services page added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import HeroComponent from "@/Components/HeroComponent";
+import Services from "@/Components/Services";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <HeroComponent/>
+      <Services/>
     </div>
   );
 }
