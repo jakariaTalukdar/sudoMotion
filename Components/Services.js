1a0736a9365c0ca375da703205ed473a518a710b
jakaria Talukdar <freelancerx2023@gmail.com>
2025-08-06 14:32:58 +0600

    services design modified

--- Components/Services.js
+++ Components/Services.js
@@ -7,26 +7,26 @@ export default function Services() {
             <h2 className="text-3xl font-bold mb-2 uppercase">OUR Services</h2>
             <div className="w-16 h-1 bg-primary mx-auto mb-10 rounded"></div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-10 gap-y-5">
             {/* Hot Beverages */}
-            <div className="flex flex-col items-center">
-                <img src="/Image/web.png" alt="Hot Beverages" className="w-40 h-40 object-contain mb-4" />
-                <h3 className="text-xl font-semibold mb-2">Web Development</h3>
-                <p className="text-alpha max-w-xs">We create fast, responsive, and SEO-optimized websites tailored to your business needs.</p>
+            <div className="flex flex-col items-start text-start border-gray-500 p-4 rounded-md bg-[#ffffff1f] backBlur">
+                <img src="/Image/web.png" alt="web development" className="size-20 md:size-32 object-contain mb-4" />
+                <h3 className="text-lg font-semibold mb-2">Web Development</h3>
+                <p className="text-alpha max-w-xs text-sm">We create fast, responsive, and SEO-optimized websites tailored to your business needs.</p>
             </div>
 
             {/* Cold Beverages */}
-            <div className="flex flex-col items-center">
-                <img src="/Image/app.png" alt="Cold Beverages" className="w-40 h-40 object-contain mb-4" />
-                <h3 className="text-xl font-semibold mb-2">App Development</h3>
-                <p className="text-alpha max-w-xs">Build cross-platform mobile apps with stunning UI and great performance using Flutter or React Native.</p>
+            <div className="flex flex-col items-start text-start border-gray-500 p-4 rounded-md bg-[#ffffff1f] backBlur">
+                <img src="/Image/app.png" alt="Cold Beverages" className="size-20 md:size-32 object-contain mb-4" />
+                <h3 className="text-lg font-semibold mb-2">App Development</h3>
+                <p className="text-alpha max-w-xs text-sm">Build cross-platform mobile apps with stunning UI and great performance using Flutter or React Native.</p>
             </div>
 
             {/* Refreshment */}
-            <div className="flex flex-col items-center">
-                <img src="/Image/video.png" alt="Refreshment" className="w-40 h-40 object-contain mb-4" />
-                <h3 className="text-xl font-semibold mb-2">Video Editing</h3>
-                <p className="text-alpha max-w-xs">From promotional videos to YouTube editing — we craft engaging stories that captivate your audience.</p>
+            <div className="flex flex-col items-start text-start border-gray-500 p-4 rounded-md bg-[#ffffff1f] backBlur">
+                <img src="/Image/video.png" alt="Refreshment" className="size-20 md:size-32 object-contain mb-4" />
+                <h3 className="text-lg font-semibold mb-2">Video Editing</h3>
+                <p className="text-alpha max-w-xs text-sm">From promotional videos to YouTube editing — we craft engaging stories that captivate your audience.</p>
             </div>
             </div>
         </div>
