1635b5779e5b09fe42857829f05e8329b8d44fda
jakaria Talukdar <freelancerx2023@gmail.com>
2025-08-06 13:34:46 +0600

    new component linked

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import About from "@/Components/About";
 import HeroComponent from "@/Components/HeroComponent";
 import Services from "@/Components/Services";
+import WhyChoseUs from "@/Components/WhyChoseUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,8 @@ export default function Home() {
       <HeroComponent/>
       <Services/>
       <About/>
+      <WhyChoseUs/>
+      
     </div>
   );
 }
