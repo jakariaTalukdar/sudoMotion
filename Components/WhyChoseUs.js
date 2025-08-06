97677b6db0050865898f6052889e04746c6900f0
jakaria Talukdar <freelancerx2023@gmail.com>
2025-08-06 13:35:20 +0600

    why choose us component added

---
+++ Components/WhyChoseUs.js
@@ -0,0 +1,35 @@
+import { whyChooseUs } from '@/lib/whyChooseUs'
+import React from 'react'
+
+export default function WhyChoseUs() {
+  return (
+    <section className="bg-[#0f0f0f] text-white py-16 bg-glove">
+        <div className="max-w-6xl mx-auto px-4">
+            <h2 className="text-3xl md:text-4xl font-bold text-center mb-4">Why Choose Us</h2>
+            <p className="text-center text-gray-400 mb-12 max-w-3xl mx-auto">
+            We combine strategy, creativity, and technology to deliver solutions that stand out. Our proven process ensures every project is delivered with excellence.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+            
+                {whyChooseUs.map((item)=>(
+                    <div key={item.id} className="flex items-start gap-5 p-2 rounded-md shadow-md bg-[#ffffff2f] backBlur">
+                        <div className={`${item.bg} p-2 rounded-lg`}>
+                            {item.svg}
+                        </div>
+                        <div>
+                            <h3 className="text-base font-semibold mb-1">{item.title}</h3>
+                            <p className="text-gray-200 text-sm">{item.description}</p>
+                        </div>
+                    </div>
+                ))}
+
+                
+
+
+            </div>
+        </div>
+    </section>
+
+  )
+}
