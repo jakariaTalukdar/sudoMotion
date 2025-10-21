21942d51ef64a08d06cba1f02896c4311f6b5493
jakariaTalukdar <freelancerx2023@gmail.com>
2025-10-21 09:37:22 +0600

    animation component used in about page

--- Components/About.js
+++ Components/About.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Reveal from '@/Components/Reveal'
 
 export default function About() {
   return (
@@ -8,18 +9,18 @@ export default function About() {
         <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center gap-10">
             
             {/* Left: Circular Image */}
-            <div className="flex-shrink-0">
-            <Image
-                height={1000}
-                width={1000}
-                src="/Image/about.jpg"
-                alt="About Us"
-                className="w-64 h-64 object-cover rounded-full border-4 border-white shadow-lg"
-            />
-            </div>
+            <Reveal className="flex-shrink-0" delay={0}>
+              <Image
+                  height={1000}
+                  width={1000}
+                  src="/Image/about.jpg"
+                  alt="About Us"
+                  className="w-64 h-64 object-cover rounded-full border-4 border-white shadow-lg"
+              />
+            </Reveal>
 
             {/* Right: Content */}
-            <div className="text-center md:text-left relative">
+            <Reveal delay={120} className="text-center md:text-left relative">
                 <h2 className="text-3xl font-bold text-white">ABOUT US</h2>
                 <div className={`flex items-center justify-center md:justify-start mb-2 gap-x-1`}>
                     <div className='top-[50px] w-20 h-1 rounded-lg bg-dark'></div>
@@ -33,7 +34,7 @@ export default function About() {
                 <div className='mt-5'>
                     <Link href={'/about'} className='transparent-button'>Work Process</Link>
                 </div>
-            </div>
+            </Reveal>
         </div>
     </section>
 
